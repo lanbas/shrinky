4143576166547a42928d8b930e48b17a34ad0010
Lance Bassett <lanbas@umich.edu>
2025-05-28 19:14:14 -0400

    Add shrinky main driver loop

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,9 @@
 #include <SDL2/SDL_mixer.h>
 
 #include "utils.h"
-#include "jumper.h"
+#include "shrinky.h"
 
 using namespace std::chrono_literals;
-
-bool running = false;
-std::mutex obstacleLock;
-std::condition_variable cv;
 
 void drawGameOver(TTF_Font* font, SDL_Renderer* renderer)
 {
@@ -19,84 +15,34 @@ void drawGameOver(TTF_Font* font, SDL_Renderer* renderer)
 
 	int width, height;
 	SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
-	rect.x = kWindowWidth / 2; 
-	rect.y = kWindowHeight / 2; 
+	rect.x = (WINDOW_WIDTH / 2) - width / 2; 
+	rect.y = 10; 
 	rect.w = width;
 	rect.h = height;
 
 	SDL_RenderCopy(renderer, texture, nullptr, &rect);
 }
 
-void printObstacle(Obstacle& o)
+void drawStrikes(uint8_t numStrikes, TTF_Font* font, SDL_Renderer* renderer, uint32_t topLeftX, uint32_t topLeftY)
 {
-	std::cout << "============\n";
-	std::cout << "Position: (" << o.position().x << ", " <<  o.position().y << ")\n";
-	std::cout << "============\n";
-}
-
-void printObstacleList(std::deque<Obstacle>& obstacleList)
-{
-	for (auto& o : obstacleList)
+	std::string strikeStr = "";
+	for (int i = 0; i < numStrikes; i++)
 	{
-		printObstacle(o);
+		strikeStr += "X";
 	}
-}
+	
+	SDL_Rect rect{};
+	SDL_Surface* surface = TTF_RenderText_Solid(font, strikeStr.c_str(), {0xFF, 0xFF, 0xFF, 0xFF});
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-float randomFloatInRange(float min, float max)
-{
-	return min + static_cast<float>(rand()) / ( static_cast<float>(RAND_MAX / (max - min)) );
-}
+	int width, height;
+	SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
+	rect.x = topLeftX; 
+	rect.y = topLeftY; 
+	rect.w = width;
+	rect.h = height;
 
-void spawnObstacles(std::deque<Obstacle>& obstacleList, std::chrono::_V2::system_clock::time_point startTime)
-{
-	float obstacleVelocityX = kInitialObstacleVelocity;
-	float obstacleWidthFactorMin = kInitialObstacleWidthFactorMin;
-	float obstacleWidthFactorMax = kInitialObstacleWidthFactorMax;
-	uint32_t obstacleSpawnInterval_ms = kInitialObstacleSpawnInterval_ms;
-
-	while (running)
-	{
-		// Determine total time elapsed
-		auto now = std::chrono::high_resolution_clock::now();
-		auto totalTimeElapsed_s = std::chrono::duration<float, std::chrono::seconds::period>(now - startTime).count();
-
-		// Velocity and object width distribution factor increase until steady state is reached
-		if (totalTimeElapsed_s < kSteadyStateTimeElapsed_s)
-		{
-			obstacleVelocityX += kObstacleVelocityIncrement;
-			obstacleWidthFactorMin += kWidthFactorIncrement;
-			obstacleWidthFactorMax += kWidthFactorIncrement;
-			obstacleSpawnInterval_ms += kObstacleSpawnIntervalIncrement_ms;
-		}
-
-		// Determine obstacle width
-		float obstacleWidthFactor = randomFloatInRange(obstacleWidthFactorMin, obstacleWidthFactorMax);
-		float obstacleWidth = obstacleWidthFactor * kObstacleUnitWidth_px;
-		
-		// Initialize as jump obstacle, kDuckObstacleProportion chance to change to duck obstacle
-		float obstaclePositionY;
-		float obstacleHeight;
-		if (randomFloatInRange(0.0f, 1.0f) < kDuckObstacleProportion) // Duck obstacle
-		{
-			obstaclePositionY = 0;
-			obstacleHeight = randomFloatInRange(kJumperHomeY, kJumperHomeY + (kJumperHeight * (1 - (1.0 / kDuckHeightDivisor))) - 2);
-		}
-		else // Jump obstacle
-		{
-			// Obstacles can be up to 1.5 * the height of the jumper
-			obstaclePositionY = randomFloatInRange(kJumperHomeY - (kJumperHeight / 2), kJumperHomeY);
-			obstacleHeight = kFloorHeight - obstaclePositionY;
-		}
-
-		Obstacle obstacle(obstaclePositionY, {obstacleVelocityX, 0}, obstacleHeight, obstacleWidth);
-
-		std::unique_lock<std::mutex> g(obstacleLock);
-		obstacleList.push_back(obstacle);
-		// std::cout << "Adding obstacle\n";
-		float spawnDeviation_ms = randomFloatInRange(-1 * kObstacleSpawnIntervalDeviation_ms, kObstacleSpawnIntervalDeviation_ms);
-		cv.wait_for(g, std::chrono::milliseconds(obstacleSpawnInterval_ms + (int)spawnDeviation_ms));
-
-	}
+	SDL_RenderCopy(renderer, texture, nullptr, &rect);
 }
 
 int main(int argc, char** argv)
@@ -105,7 +51,7 @@ int main(int argc, char** argv)
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     TTF_Init();
 
-	SDL_Window* window = SDL_CreateWindow("Jumper", 0, 0, kWindowWidth, kWindowHeight, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("Shrinky", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0); 
 
     // Initialize the font
@@ -117,25 +63,32 @@ int main(int argc, char** argv)
 	// Mix_Chunk* wallHitSound = Mix_LoadWAV("audio/pongWallHit.wav");
 	// Mix_Chunk* paddleHitSound = Mix_LoadWAV("audio/pongPaddleHit.wav");
 
-    // Create Jumper (starts at configured defaults)
-    Jumper jumper;
+    // Create Drainer and Grid
+	Drainer drainer(0, 0, config.gridWidth_cells, config.gridHeight_cells);
+    Grid grid(config.gridWidth_cells, config.gridHeight_cells, drainer);
+	Score totalScore(gameFont, {10, 10});
+
 	auto globalStartTime = std::chrono::high_resolution_clock::now();
 	auto now = std::chrono::high_resolution_clock::now();
 	auto totalTimeElapsed_ms = std::chrono::duration<float, std::chrono::milliseconds::period>(now - globalStartTime).count();
+	float lastFillTime_ms = 0.0f;
+	float lastFillPeriodUpdate_ms = 0.0f;
+	float lastDrainRatePeriodUpdate_ms = 0.0f;
+	uint8_t strikes = 0;
+	bool gameOver = false;
 
     // Game logic
     {
-        running = true;
-		
+        bool running = true;
 		float dt = 0.0f;
-		std::deque<Obstacle> obstacleList;
-		std::thread t(&spawnObstacles, std::ref(obstacleList), globalStartTime);
 
         // Continue looping and processing events until user exits
 		while (running)
 		{
 			auto startTime = std::chrono::high_resolution_clock::now();
 			SDL_Event event;
+			float score = 0.0f;
+
 			while (SDL_PollEvent(&event))
 			{
 				if (event.type == SDL_QUIT)
@@ -152,77 +105,69 @@ int main(int argc, char** argv)
 							running = false;
 							break;
 						case SDLK_SPACE:
-							if (jumper.state() == JumperState::RUNNING)
-                            {
-                                jumper.jump();
-                            }
+						{
+							GridPosition drainerPosition = drainer.position(); 
+							bool wasFull = grid.drainCell(drainerPosition.row, drainerPosition.col, score);
+							totalScore.addScore((int)score);
+							if (!wasFull)
+							{
+								strikes++;
+							}
 							break;
-						case SDLK_s:
-							if (jumper.state() == JumperState::RUNNING)
-                            {
-                                jumper.duck();
-                            }
-							break;
+						}
 						case SDLK_DOWN:
-							if (jumper.state() == JumperState::RUNNING)
-                            {
-                                jumper.duck();
-                            }
+							drainer.move(PlayerMove::DOWN);
+							break;
+						case SDLK_UP:
+							drainer.move(PlayerMove::UP);
+							break;
+						case SDLK_LEFT:
+							drainer.move(PlayerMove::LEFT);
+							break;
+						case SDLK_RIGHT:
+							drainer.move(PlayerMove::RIGHT);
 							break;
 					}
 				}
-				else if (event.type == SDL_KEYUP)
-				{
-					switch(event.key.keysym.sym)
-					{
-						case SDLK_s:
-                            if (jumper.state() == JumperState::DUCKING)
-							    jumper.reset();
-							break;
-						case SDLK_DOWN:
-							if (jumper.state() == JumperState::DUCKING)
-							    jumper.reset();
-							break;
-                    }
-                }
+			}
+
+			// Update fill frequency and rate
+			if (totalTimeElapsed_ms - lastFillPeriodUpdate_ms > config.fillIntervalDeltaPeriod_ms)
+			{
+				lastFillPeriodUpdate_ms = totalTimeElapsed_ms;
+				config.fillInterval_ms = std::max(config.fillIntervalMin_ms, config.fillInterval_ms - config.fillIntervalDelta_ms);
+			}
+
+			if (totalTimeElapsed_ms - lastDrainRatePeriodUpdate_ms > config.drainRateDeltaPeriod_ms)
+			{
+				lastDrainRatePeriodUpdate_ms = totalTimeElapsed_ms;
+				config.drainRate = std::min(config.drainRateMax, config.drainRate + config.drainRateDelta);
+			}
+
+			// If enough time has passed, fill a cell on the grid
+			if (totalTimeElapsed_ms - lastFillTime_ms > config.fillInterval_ms)
+			{
+				lastFillTime_ms = totalTimeElapsed_ms;
+				grid.fillCell(config.drainRate);
 			}
 
             // Clear the window to black
 			SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xFF);
 			SDL_RenderClear(renderer);
 
-            // Set the draw color to be white
-            SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+			// Update and draw game
+			strikes += grid.update(dt);
 
-			// Update objects and score
-			score.setScore(totalTimeElapsed_ms / 100);
+			// Draw grid()
+			grid.draw(renderer);
+			totalScore.draw(renderer);
+			drawStrikes(std::min(3, (int)strikes), gameFont, renderer, 0.9 * WINDOW_WIDTH, 10);
 
-			jumper.updateMotion(dt);
-
-			{
-				// Remove front if necessary, then update + draw remaining
-				std::unique_lock<std::mutex> g(obstacleLock);
-				if (!obstacleList.empty() && obstacleList.front().right() <= 0)
-				{
-					// Timing is such that only one obstacle (the front one) will need to be deleted on any given run of this loop
-					obstacleList.pop_front();
-				}
-
-				for (auto& obstacle : obstacleList)
-				{
-					// Nice to only iterate once, but idk if there is a benefit to drawing all at once instead of sneaking in an updateMotion
-					obstacle.updateMotion(dt);
-					obstacle.draw(renderer);
-				}
-			}
-
-			jumper.draw(renderer);
-			score.draw(renderer);
-
-			// If jumper is colliding with object, game is over
-			if (jumper.isColliding({obstacleList.begin(), obstacleList.end()}))
+			if (strikes >= 3)
 			{
 				running = false;
+				gameOver = true;
+				drawGameOver(gameFont, renderer);
 			}
 
 			// Present the backbuffer
@@ -234,13 +179,8 @@ int main(int argc, char** argv)
 			totalTimeElapsed_ms += dt;
         }
 
-		cv.notify_one();
-		t.join();
-
-		drawGameOver(gameFont, renderer);
-		SDL_RenderPresent(renderer);
-		std::this_thread::sleep_for(10s);
-
+		if (gameOver)
+			std::this_thread::sleep_for(5s);
     }
 
     return 0;
